c34a4c34d126e59d610201c6525e20d52d94573f
Sanjay <sanjusadasivam890@gmail.com>
2024-02-28 06:08:49 +0530

    created main page of register

---
+++ lib/pages/register/register.dart
@@ -0,0 +1,72 @@
+import 'package:courseapp/pages/common_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: buildAppBar("Sign Up"),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                    child: reusableText(
+                        "Enter your details below and & free sign up")),
+                Container(
+                  margin: EdgeInsets.only(top: 36.h),
+                  padding: EdgeInsets.only(left: 25.w, right: 25.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      reusableText("User Name"),
+                      buildTextField("Enter your user name", "name", "user",
+                          (value) {
+                        //context.read<SignInBlocs>().add(EmailEvent(value));
+                      }),
+                      reusableText("Email"),
+                      buildTextField(
+                          "Enter your email address", "email", "user", (value) {
+                        //context.read<SignInBlocs>().add(EmailEvent(value));
+                      }),
+                      reusableText("Password"),
+                      buildTextField("Enter your password", "password", "lock",
+                          (value) {
+                        //context.read<SignInBlocs>().add(PasswordEvent(value));
+                      }),
+                      reusableText("Re-enter Password"),
+                      buildTextField(
+                          "Confirm your password", "password", "lock", (value) {
+                        //context.read<SignInBlocs>().add(PasswordEvent(value));
+                      }),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 25.w),
+                  child: reusableText(
+                      "By creating an account you have to agree with oru terms and condition."),
+                ),
+                buildLoginAndRegButton("Sign Up", "login", () {
+                  Navigator.of(context).pushNamed("register");
+                }),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
